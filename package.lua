7ff9c06c8d4e287fe95e01de39e327fe39bfabab
Lautenschlager <tai.romani@hotmail.com>
2021-02-25 00:37:17 -0300

    blabla

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+  name = "staff801secretary",
+  version = "1.0.0",
+  description = "bla bla bla",
+  dependencies = {
+    "luvit/luvit" -- here you write all the dependencies, in this case, luvit only
+  },
+}
