31df7a86c36c35abb1d197149d9dfd0c6ab05e49
Lautenschlager <tai.romani@hotmail.com>
2021-03-12 02:03:37 -0300

    rewrite secretary, it's von drekkemetrics now!

--- src/utils/discord-objects.lua
+++ src/utils/discord-objects.lua
@@ -2,8 +2,8 @@ local discord = require("../discord").discord
 
 local channels = {
 	-- Int Staff
-	["int-main"] = "818016844522586123",--"800100513718075414", -- #staff-utils
-	["int-breach"] = "818016844522586123",--"814497990948814918", -- #data-breaches
+	["int-main"] = "800100513718075414", -- #staff-utils
+	["int-breach"] = "814497990948814918", -- #data-breaches
 
 	-- Br Staff
 	["br-flood"] = "818016844522586123",--"450616585065857024", -- #bot-uso
