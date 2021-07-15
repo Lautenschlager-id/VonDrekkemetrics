8a58645462cdccfcd19bb8c1450cefe06b86ae82
Lautenschlager <tai.romani@hotmail.com>
2021-07-15 15:11:07 -0300

    remove 255 characters restriction

--- src/commands/farm.lua
+++ src/commands/farm.lua
@@ -10,9 +10,6 @@ local table_concat = table.concat
 
 ----------------------------------------------------------------------------------------------------
 
-local banCommand = "/banhack "
-local messageLength = 255 - #banCommand + 1
-
 return {
 	channel = {
 		[discordChannels["br-mod-utils"].id] = true,
@@ -26,29 +23,12 @@ return {
 	execute = function(self, message, parameters)
 		if not parameters then return end
 
-		local names, mainIndex, subIndex = { { _len = 0 } }, 1, 0
+		local names, index = { }, 0
 		for name in str_gmatch(parameters, "(%S+#%d%d%d%d) / #") do
-			local current = names[mainIndex]
-
-			local len = current._len + #name + 1
-			if len <= messageLength then
-				current._len = len
-
-				subIndex = subIndex + 1
-				current[subIndex] = name
-			else
-				subIndex = 1
-				mainIndex = mainIndex + 1
-				names[mainIndex] = { _len = #name + 1, name }
-			end
+			index = index + 1
+			names[index] = name
 		end
 
-		local messages, msgIndex = { }, 0
-		for msg = 1, mainIndex do
-			msgIndex = msgIndex + 1
-			messages[msgIndex] = message:reply(table_concat(names[msg], ','))
-		end
-
-		temporaryObject[message.id] = messages
+		temporaryObject[message.id] = message:reply(table_concat(names, ','))
 	end
 }
