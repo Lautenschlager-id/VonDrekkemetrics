ce46cff9698be4ba4e99d2ed021b2ebd72f6d3df
Lautenschlager <tai.romani@hotmail.com>
2022-04-14 22:29:23 -0300

    fix sub with utf8

--- src/utils/extensions/string.lua
+++ src/utils/extensions/string.lua
@@ -1,9 +1,15 @@
 ------------------------------------------- Optimization -------------------------------------------
 
+local bit_rshift = bit.rshift
+
+local str_byte = string.byte
 local str_find = string.find
+local str_format = string.format
 local str_gmatch = string.gmatch
 local str_gsub = string.gsub
 local str_sub = string.sub
+
+local tbl_concat = table.concat
 
 local tonumber = tonumber
 
@@ -28,6 +34,53 @@ string.split = function(str, separator, raw)
 	out[counter] = tonumber(out[counter]) or out[counter]
 
 	return out, counter
+end
+
+do
+	local charLength = function(byte)
+		if bit_rshift(byte, 7) == 0x00 then
+			return 1
+		elseif bit_rshift(byte, 5) == 0x06 then
+			return 2
+		elseif bit_rshift(byte, 4) == 0x0E then
+			return 3
+		elseif bit_rshift(byte, 3) == 0x1E then
+			return 4
+		end
+		return 0
+	end
+
+	string.utf8 = function(str)
+		local utf8str = { }
+		local index, append = 1, 0
+
+		local charLen
+
+		for i = 1, #str do
+			repeat
+				local char = str_sub(str, i, i)
+				local byte = str_byte(char)
+				if append ~= 0 then
+					utf8str[index] = utf8str[index] .. char
+					append = append - 1
+
+					if append == 0 then
+						index = index + 1
+					end
+					break
+				end
+
+				charLen = charLength(byte)
+				utf8str[index] = char
+				if charLen == 1 then
+					index = index + 1
+				end
+				append = append + charLen - 1
+			until true
+		end
+
+		return utf8str
+	end
 end
 
 local splitByLine = function(content, max)
@@ -57,9 +110,7 @@ local strAutoEllipsis = function(str, maxLength)
 		return str
 	end
 
-	str = str_sub(str, 1, maxLength - 3)
-	str = str_gsub(str, "\\%d+", '')
-
+	str = tbl_concat(string.utf8(str), '', 1, maxLength - 3)
 	return str .. "..."
 end
 
