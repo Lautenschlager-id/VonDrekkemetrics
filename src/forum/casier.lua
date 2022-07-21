7f8590afa6117057bbe10f9965b15d50d9f32b7b
Lautenschlager <tai.romani@hotmail.com>
2022-07-20 21:58:45 -0300

    '.'

--- src/forum/casier.lua
+++ src/forum/casier.lua
@@ -18,8 +18,8 @@ local accordionTitle = "<thead>(.-)</thead>.-<tbody>(.-)</tbody>"
 local getForumAvatarCasier = function(playerName)
 	local html = forum.getPage(str_format(uri, utils.readableToEncodedNickname(playerName)))
 
-	local bodyItems, error = activity.getAccordion(html, accordionTitle, "avatarOnly", 0, playerName,
-		"activeSanctions")
+	local bodyItems, error = activity.getAccordion(html, accordionTitle, "avatarOnly", 0,
+		playerName, "activeSanctions")
 
 	return bodyItems, error
 end
